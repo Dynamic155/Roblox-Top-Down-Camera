f2e7daf8d27bd944bd0c5152bc0abcb1d8c90e5c
Dynamic <79714443+Dynamic155@users.noreply.github.com>
2024-06-01 15:39:56 +0200

    Create CameraScript.lua

---
+++ CameraScript.lua
@@ -0,0 +1,22 @@
+-- Options:
+local cameraHeight = 25
+local cameraOffsetX = 0
+local cameraOffsetZ = 0
+
+-- Variables:
+local players = game:GetService("Players")
+local runService = game:GetService("RunService")
+local tweenSerive = game:GetService("TweenService")
+
+local player = players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local camera = game.Workspace.CurrentCamera
+camera.CameraType = Enum.CameraType.Scriptable
+
+-- Camera Script:
+local function updateCamera()
+	local targetPosition = character.PrimaryPart.Position + Vector3.new(cameraOffsetX, cameraHeight, cameraOffsetZ)
+	tweenSerive:Create(camera, TweenInfo.new(0.1), {CFrame = CFrame.new(targetPosition, character.PrimaryPart.Position)}):Play()
+end
+
+runService.RenderStepped:Connect(updateCamera)
